c37504fa92cb99f9a6d9f1554590ac4f48f23d59
hmbashar <hmbashar@gmail.com>
2024-12-16 15:41:45 +0600

    added fields for how many categories will be show for category title

--- includes/widgets/category-title-widget.php
+++ includes/widgets/category-title-widget.php
@@ -6,32 +6,60 @@ use Elementor\Controls_Manager;
 use Elementor\Group_Control_Typography;
 use Elementor\Group_Control_Text_Shadow;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-class Category_Title_Widget extends Widget_Base {
-    public function get_name() {
+class Category_Title_Widget extends Widget_Base
+{
+    public function get_name()
+    {
         return 'category_title';
     }
 
-    public function get_title() {
-        return esc_html__( 'Category Title', 'smart-category-addons' );
+    public function get_title()
+    {
+        return esc_html__('Category Title', 'smart-category-addons');
     }
 
-    public function get_icon() {
+    public function get_icon()
+    {
         return 'eicon-editor-bold';
     }
 
-    public function get_categories() {
-        return [ 'theme-elements-single' ];
+    public function get_categories()
+    {
+        return ['theme-elements-single'];
     }
-    protected function register_controls() {
+    protected function register_controls()
+    {
+        // Content Tab
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__('Settings', 'smart-category-addons'),
+                'tab' => Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'category_limit',
+            [
+                'label' => esc_html__('Number of Categories to Show', 'smart-category-addons'),
+                'type' => Controls_Manager::NUMBER,
+                'default' => 3,
+                'min' => 1,
+                'step' => 1,
+                'description' => esc_html__('Set how many categories to display. If the post has fewer categories, all available categories will be shown.', 'smart-category-addons'),
+            ]
+        );
+
+        $this->end_controls_section();
         // Style Tab
         $this->start_controls_section(
             'style_section',
             [
-                'label' => esc_html__( 'Category Title', 'smart-category-addons' ),
+                'label' => esc_html__('Category Title', 'smart-category-addons'),
                 'tab' => Controls_Manager::TAB_STYLE,
             ]
         );
@@ -40,7 +68,7 @@ class Category_Title_Widget extends Widget_Base {
         $this->add_control(
             'category_title_color',
             [
-                'label' => esc_html__( 'Text Color', 'smart-category-addons' ),
+                'label' => esc_html__('Text Color', 'smart-category-addons'),
                 'type' => Controls_Manager::COLOR,
                 'selectors' => [
                     '{{WRAPPER}} .category-title a' => 'color: {{VALUE}};',
@@ -51,7 +79,7 @@ class Category_Title_Widget extends Widget_Base {
         $this->add_control(
             'category_title_hover_color',
             [
-                'label' => esc_html__( 'Hover Color', 'smart-category-addons' ),
+                'label' => esc_html__('Hover Color', 'smart-category-addons'),
                 'type' => Controls_Manager::COLOR,
                 'selectors' => [
                     '{{WRAPPER}} .category-title a:hover' => 'color: {{VALUE}};',
@@ -63,7 +91,7 @@ class Category_Title_Widget extends Widget_Base {
             Group_Control_Typography::get_type(),
             [
                 'name' => 'category_title_typography',
-                'label' => esc_html__( 'Typography', 'smart-category-addons' ),
+                'label' => esc_html__('Typography', 'smart-category-addons'),
                 'selector' => '{{WRAPPER}} .category-title a',
             ]
         );
@@ -73,7 +101,7 @@ class Category_Title_Widget extends Widget_Base {
             Group_Control_Text_Shadow::get_type(),
             [
                 'name' => 'category_title_text_shadow',
-                'label' => esc_html__( 'Text Shadow', 'smart-category-addons' ),
+                'label' => esc_html__('Text Shadow', 'smart-category-addons'),
                 'selector' => '{{WRAPPER}} .category-title a',
             ]
         );
@@ -82,9 +110,9 @@ class Category_Title_Widget extends Widget_Base {
         $this->add_responsive_control(
             'category_title_margin',
             [
-                'label' => esc_html__( 'Margin', 'smart-category-addons' ),
+                'label' => esc_html__('Margin', 'smart-category-addons'),
                 'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => [ 'px', '%', 'em' ],
+                'size_units' => ['px', '%', 'em'],
                 'selectors' => [
                     '{{WRAPPER}} .category-title' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
                 ],
@@ -95,9 +123,9 @@ class Category_Title_Widget extends Widget_Base {
         $this->add_responsive_control(
             'category_title_padding',
             [
-                'label' => esc_html__( 'Padding', 'smart-category-addons' ),
+                'label' => esc_html__('Padding', 'smart-category-addons'),
                 'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => [ 'px', '%', 'em' ],
+                'size_units' => ['px', '%', 'em'],
                 'selectors' => [
                     '{{WRAPPER}} .category-title a' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
                 ],
@@ -108,10 +136,16 @@ class Category_Title_Widget extends Widget_Base {
     }
 
     protected function render() {
+        $settings = $this->get_settings_for_display();
+        $category_limit = ! empty( $settings['category_limit'] ) ? (int) $settings['category_limit'] : 3;
+
         // Get the current post's categories
         $categories = get_the_category();
-        
+
         if ( ! empty( $categories ) && is_array( $categories ) ) {
+            // Limit the categories to the user-defined number
+            $categories = array_slice( $categories, 0, $category_limit );
+
             echo '<p class="category-title">';
             foreach ( $categories as $category ) {
                 // Escape output for security
